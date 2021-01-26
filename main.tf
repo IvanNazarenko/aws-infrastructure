82c405b62942503c71c37edce4e77825edb7b710
IvanNavarenko <severinoc4rd0z0@gmail.com>
2021-01-26 16:17:25 +0200

    Add bucket

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+}
+
+variable "bucket_name" {
+  type = string
+  description = "neme of bucket"
+}
+resource "aws_s3_bucket" "vidro" {
+  bucket = "var.bucket_name"
+  acl = "private"
+}
