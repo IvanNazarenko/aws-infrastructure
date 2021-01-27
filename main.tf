2fd9ede43a02ff017df7919e173272e7beb3c13b
IvanNavarenko <severinoc4rd0z0@gmail.com>
2021-01-27 15:14:25 +0200

    My infrastructure

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ variable "bucket_name" {
   description = "neme of bucket"
 }
 
-#resource "aws_s3_bucket" "storage" {
-#  bucket = var.bucket_name
-#  tags = {
-#    target = "learning"
-#  }
-#}
+resource "aws_s3_bucket" "storage" {
+  bucket = var.bucket_name
+  tags = {
+    target = "learning"
+  }
+}
 
 #resource "aws_s3_bucket_object" "bootstrap" {
 #  bucket = var.bucket_name
@@ -19,44 +19,44 @@ variable "bucket_name" {
 #  source = "bootstrap.sh"
 #}
 
-#resource "aws_security_group" "open-ssh" {
-#  name = "open-ssh"
-#  description = "ssh from anywhere"
-#  ingress {
-#    from_port = 0
-#    protocol = "tcp"
-#    to_port = 22
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#  egress {
-#    from_port = 0
-#    protocol = "-1"
-#    to_port = 0
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#  tags = {
-#    target = "learning"
-#  }
-#}
+resource "aws_security_group" "open-ssh" {
+  name = "open-ssh"
+  description = "ssh from anywhere"
+  ingress {
+    from_port = 0
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    target = "learning"
+  }
+}
 
-#resource "aws_key_pair" "wayne" {
-#  key_name = "virginia"
-#  public_key = file("id_rsa.pub")
-#}
+resource "aws_key_pair" "wayne" {
+  key_name = "virginia"
+  public_key = file("id_rsa.pub")
+}
 
-#resource "aws_instance" "vm-for-moodle" {
-#  ami = "ami-0885b1f6bd170450c"
-#  instance_type = "t2.micro"
-#  user_data = file("bootstrap.sh")
-#  key_name = aws_key_pair.wayne.key_name
-#  root_block_device {
-#    delete_on_termination = true
-#    volume_size = 8
-#    volume_type = "gp2"
-#  }
-#  tags = {
-#   target = "learning"
-#    Name = moodle
-#  }
-#  security_groups = ["open-ssh"]
-#}
+resource "aws_instance" "vm-for-moodle" {
+  ami = "ami-0885b1f6bd170450c"
+  instance_type = "t2.micro"
+  user_data = file("bootstrap.sh")
+  key_name = aws_key_pair.wayne.key_name
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 8
+    volume_type = "gp2"
+  }
+  tags = {
+   target = "learning"
+    Name = moodle
+  }
+  security_groups = ["open-ssh"]
+}
