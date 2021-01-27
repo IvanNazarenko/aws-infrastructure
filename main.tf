8eec5163b1039fee750dcf20ce162995ab66959b
IvanNavarenko <severinoc4rd0z0@gmail.com>
2021-01-27 10:33:19 +0200

    remove bucket

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ variable "bucket_name" {
   type = string
   description = "neme of bucket"
 }
-resource "aws_s3_bucket" "vidro" {
-  bucket = var.bucket_name
-}
+#resource "aws_s3_bucket" "vidro" {
+#  bucket = var.bucket_name
+#}
