30add8311b5c27b5bea7f20c139ff8ae8488b52f
IvanNavarenko <severinoc4rd0z0@gmail.com>
2021-01-27 12:10:54 +0200

    added ec2 + bootstraping , sec group, key pai,s3 bucket

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "---------------START------------"
+#mkdir /home/root/s3
+#aws s3 sync s3://my-storage-872122 /home/root/s3
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker pull  inazarenko/myapp-nodejs:latest
+sudo docker run -id inazarenko/myapp-nodejs
+echo "UserData executed on $(date)" >> /var/www/html/log.txt
+echo "--------------FINISH------------"
--- main.tf
+++ main.tf
@@ -5,6 +5,45 @@ variable "bucket_name" {
   type = string
   description = "neme of bucket"
 }
-#resource "aws_s3_bucket" "vidro" {
-#  bucket = var.bucket_name
-#}
+resource "aws_s3_bucket" "storage" {
+  bucket = var.bucket_name
+  source ="${file("bootstrap.sh")}"
+  tags = {
+    Username = learning
+  }
+}
+
+resource "aws_security_group" "open-ssh" {
+  name = "open-ssh"
+  description = "ssh from anywhere"
+  ingress {
+    from_port = 22
+    protocol = "SSH"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Username = learning
+  }
+}
+
+resource "aws_key_pair" "wayne" {
+  key_name = "virginia"
+  public_key = "ssh-rsa"
+}
+
+resource "aws_instance" "vm-for-moodle" {
+  ami = "ami-0885b1f6bd170450c"
+  instance_type = "t2.micro"
+  user_data ="${file("bootstrap.sh")}"
+  key_name = "${aws_key_pair.wayne.key_name}"
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 8
+    volume_type = "General Purpose SSG (gp2)"
+  }
+  tags = {
+    Username = learning
+  }
+  security_groups = ["open-ssh"]
+}
